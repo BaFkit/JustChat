463d9c17299662938df723bb660e4bc9b4250a97
bafkit <vpverynok@mail.ru>
2021-11-19 00:39:31 +0300

    HomeWork_Lesson4

---
+++ src/main/java/JustChat/ChatController.java
@@ -0,0 +1,22 @@
+package JustChat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ChatController {
+    @FXML
+    TextArea mainTextArea;
+    @FXML
+    TextField mainTextField;
+
+    @FXML
+    protected void onButtonClick() {
+        if(!mainTextField.getText().isEmpty()){
+            mainTextArea.appendText(mainTextField.getText().trim() + "\n");
+            mainTextField.clear();
+            }
+        }
+    }
+
+
