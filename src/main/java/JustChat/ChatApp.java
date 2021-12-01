cb50f8e5d316cc71b99a6a710057a2afdb8a6726
bafkit <vpverynok@mail.ru>
2021-12-02 00:05:34 +0300

    Private messages are working.

--- src/main/java/JustChat/ChatApp.java
+++ src/main/java/JustChat/ChatApp.java
@@ -1,9 +1,11 @@
 package JustChat;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
 
@@ -16,6 +18,12 @@ public class ChatApp extends Application {
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+            @Override
+            public void handle(WindowEvent windowEvent) {
+
+            }
+        });
     }
 
     public static void main(String[] args) {
--- src/main/java/JustChat/ChatController.java
+++ src/main/java/JustChat/ChatController.java
@@ -1,10 +1,23 @@
 package JustChat;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class ChatController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatController implements Initializable {
+    private Client client;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        client = new Client((msg -> {
+            mainTextArea.appendText(msg);
+        }));
+    }
+
     @FXML
     TextArea mainTextArea;
     @FXML
@@ -12,11 +25,14 @@ public class ChatController {
 
     @FXML
     protected void onButtonClick() {
-        if(!mainTextField.getText().isEmpty()){
-            mainTextArea.appendText("-" + mainTextField.getText().trim() + "\n");
+        if(!mainTextField.getText().trim().isEmpty()){
+            client.sendMessage(mainTextField.getText().trim());
             mainTextField.clear();
+            mainTextField.requestFocus();
             }
-        }
+    }
+
+
 }
 
 
